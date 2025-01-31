0c52a859734db52723816cab27049caef1a98ba1
Serhii Zadorozhnii <wiseacreserj@gmail.com>
2025-01-31 04:45:36 +0200

    responsive-table done pagination functionality

--- responsive-table/script.js
+++ responsive-table/script.js
@@ -1,5 +1,80 @@
-//Dark Mode Functionality
+const spinner = document.getElementById("spinner");
+const table = document.getElementById("data-table");
+const tableBody = document.getElementById("table-body");
+const pagination = document.getElementById("pagination");
+const prevBtn = document.getElementById("prev-btn");
+const nextBtn = document.getElementById("next-btn");
+const pageNumber = document.getElementById("page-number");
 
+let data = [];
+let currentPage = 1;
+const rowsPerPage = 10;
+
+//Fetch data from API
+
+async function fetchData() {
+    spinner.style.display = "flex";
+    try {
+        const response = await fetch("https://randomuser.me/api?results=50");
+        const json = await response.json();
+        data = json.results;
+        displayTable(data);
+        updateButtons();
+    } catch (error) {
+        console.error("Error fetching data:", error);
+    } finally {
+        spinner.style.display = "none";
+        table.style.display = "table";
+        pagination.style.display = "block";
+    }
+}
+
+//Display table data
+function displayTable(dataToDisplay) {
+    tableBody.innerText = "";
+    const start = (currentPage - 1) * rowsPerPage;
+    const end = start + rowsPerPage;
+    const paginatedItems = dataToDisplay.slice(start, end);
+    paginatedItems.forEach((user) => {
+        const row = `<tr>
+                        <td data-label="Name">${user.name.first} ${user.name.last}</td>
+                        <td data-label="Email">${user.email}</td>
+                        <td data-label="Username">${user.login.username}</td>
+                        <td data-label="Country Name">${user.location.country}</td>
+                    </tr>`;
+        tableBody.insertAdjacentHTML("beforeend", row);
+    });
+}
+
+//Previous Page
+function prevPage() {
+    if (currentPage > 1) {
+        currentPage--;
+        displayTable(data);
+        updateButtons();
+    }
+}
+
+//Next Page
+function nextPage() {
+    if (currentPage * rowsPerPage < data.length) {
+        currentPage++;
+        displayTable(data);
+        updateButtons();
+    }
+}
+
+//Update paginntion buttons
+function updateButtons() {
+    pageNumber.innerText = currentPage;
+    prevBtn.disabled = currentPage === 1;
+    nextBtn.disabled = currentPage * rowsPerPage >= data.length;
+}
+
+//Startup
+fetchData();
+
+//Dark Mode Functionality
 const themeToggle = document.getElementById("theme-toggle");
 const body = document.body;
 
