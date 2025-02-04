71b0c833f8c4d4e7a3f8982e84f7b6428da9ceba
Serhii Zadorozhnii <wiseacreserj@gmail.com>
2025-02-04 20:11:30 +0200

    quiz-game add check answer functionality

--- quiz-game/script.js
+++ quiz-game/script.js
@@ -35,12 +35,12 @@ async function fetchQuestions() {
 //Load each question to UI
 function loadQuestion() {
     const question = questions[currentQuestion];
-    questionText.innerText = question.question;
+    questionText.innerText = decodeHTML(question.question);
     questionNumber.innerText = `Question ${currentQuestion + 1}`;
 
     //Reset choices container
     choicesContainer.innerText = "";
-    //  nextBtn.disabled = true;
+    nextBtn.disabled = true;
 
     //Prepare choices
     const choices = [
@@ -48,10 +48,13 @@ function loadQuestion() {
         question.correct_answer,
     ].sort(() => Math.random() - 0.5);
     choices.forEach((choice) => {
+        choice = choice.trim();
         const button = document.createElement("button");
         button.type = "button";
         button.classList.add("choice");
-        button.innerText = choice;
+        button.innerText = decodeHTML(choice);
+        button.onclick = () =>
+            checkAnswer(choice, question.correct_answer.trim());
         choicesContainer.appendChild(button);
     });
 }
@@ -61,6 +64,30 @@ nextBtn.addEventListener("click", () => {
     currentQuestion++;
     loadQuestion();
 });
+
+//Ensure special characters are decoded
+function decodeHTML(html) {
+    const txt = document.createElement("div");
+    txt.innerHTML = html;
+    return txt.textContent;
+}
+
+//Check if answer is correct
+function checkAnswer(selectedAnswe, correctAnswer) {
+    console.log(selectedAnswe, correctAnswer);
+
+    const choices = document.querySelectorAll(".choice");
+    choices.forEach((choice) => {
+        if (choice.innerText === decodeHTML(correctAnswer)) {
+            choice.classList.add("correct");
+        } else {
+            choice.classList.add("wrong");
+        }
+        choice.disabled = true;
+    });
+
+    nextBtn.disabled = false;
+}
 
 //Startup
 fetchQuestions();
