09ff0c734119cc753faf290ce234080405ee59a8
Serhii Zadorozhnii <wiseacreserj@gmail.com>
2025-02-11 22:01:29 +0200

    podcast-player  setup env and basic imports for server.js

--- podcast-player/server.js
+++ podcast-player/server.js
@@ -1,8 +1,23 @@
 const express = require("express");
+const path = require("path");
+const dotenv = require("dotenv");
+const CryptoJS = require("crypto-js");
+const fetch = require("node-fetch");
+
+dotenv.config();
+
 const app = express();
+const PORT = process.env.PORT || 3000;
 
-app.get("/", function (req, res) {
-    res.send("Hello Serhii");
+const authKey = process.env.AUTH_KEY;
+const secretKey = process.env.SECRET_KEY;
+const userAgent = process.env.USER_AGENT;
+const apiEndpoint = process.env.API_ENDPOINT;
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.listen(PORT, () => {
+    console.log(
+        `Server is running on http://localhost:${PORT} pointing to ${apiEndpoint}`
+    );
 });
-
-app.listen(3000);
