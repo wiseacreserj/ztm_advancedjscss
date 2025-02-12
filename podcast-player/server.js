3636200b37688f7bb2dbf74211bf71da7b30b75c
Serhii Zadorozhnii <wiseacreserj@gmail.com>
2025-02-12 21:54:57 +0200

    podcast-player  fix errors in package.json, add route for search podcast bya API calling

--- podcast-player/server.js
+++ podcast-player/server.js
@@ -16,6 +16,55 @@ const apiEndpoint = process.env.API_ENDPOINT;
 
 app.use(express.static(path.join(__dirname, "public")));
 
+//Shared auth function
+function generateAuthHeaders() {
+    const apiHeaderTime = Math.floor(new Date().getTime() / 1000);
+    const hash = CryptoJS.SHA1(authKey + secretKey + apiHeaderTime).toString(
+        CryptoJS.enc.Hex
+    );
+
+    return {
+        "User-Agent": userAgent,
+        "X-Auth-Key": authKey,
+        "X-Auth-Date": apiHeaderTime.toString(),
+        Authorization: hash,
+    };
+}
+
+//Search for podcasts
+app.get("/api/search", async (req, res) => {
+    const query = req.query.q;
+    if (!query) {
+        return res.status(400).json({ error: "Query parameter is required!" });
+    }
+    const headers = generateAuthHeaders();
+
+    try {
+        const response = await fetch(
+            `${apiEndpoint}/search/byterm?q=${encodeURIComponent(query)}`,
+            {
+                method: "GET",
+                headers: headers,
+            }
+        );
+
+        if (
+            response.ok &&
+            response.headers.get("content-type").includes("application/json")
+        ) {
+            const data = await response.json();
+            res.json(data);
+        } else {
+            const rawText = await response.text();
+            console.log("Raw Response:", rawText);
+            res.status(500).json({ error: "Invalid response API", rawText });
+        }
+    } catch (error) {
+        console.error("Error fetching API:", error.message);
+        res.status(500).json({ error: error.message });
+    }
+});
+
 app.listen(PORT, () => {
     console.log(
         `Server is running on http://localhost:${PORT} pointing to ${apiEndpoint}`
