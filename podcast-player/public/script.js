6f01804ffec811755dbd17700ca2dd4e9643895a
Serhii Zadorozhnii <wiseacreserj@gmail.com>
2025-02-21 17:47:46 +0200

    podcast-player  work on mq for overall resoponsive  for smarts with UI

--- podcast-player/public/script.js
+++ podcast-player/public/script.js
@@ -1,31 +1,33 @@
-// Navigation ----------------------------------
-const searchLink = document.getElementById("searchLink");
-const listenLink = document.getElementById("listenLink");
-const searchContainer = document.querySelector(".search-container");
-const mainContainer = document.querySelector(".main-container");
-const playerContainer = document.querySelector(".player-container");
-const queueContainer = document.querySelector(".queue");
+document.addEventListener("DOMContentLoaded", () => {
+    // Navigation ----------------------------------
+    const searchLink = document.getElementById("searchLink");
+    const listenLink = document.getElementById("listenLink");
+    const searchContainer = document.querySelector(".search-container");
+    const mainContainer = document.querySelector(".main-container");
+    const playerContainer = document.querySelector(".player-container");
+    const queueContainer = document.querySelector(".queue");
 
-searchLink.addEventListener("click", navigateToSearch);
-listenLink.addEventListener("click", navigateToPlayer);
+    searchLink.addEventListener("click", navigateToSearch);
+    listenLink.addEventListener("click", navigateToPlayer);
 
-function navigateToSearch() {
-    searchContainer.style.display = "flex";
-    mainContainer.style.display = "flex";
-    playerContainer.style.display = "none";
-    queueContainer.style.display = "none";
-    searchLink.classList.add("selected");
-    listenLink.classList.remove("selected");
-}
+    function navigateToSearch() {
+        searchContainer.style.display = "flex";
+        mainContainer.style.display = "flex";
+        playerContainer.style.display = "none";
+        queueContainer.style.display = "none";
+        searchLink.classList.add("selected");
+        listenLink.classList.remove("selected");
+    }
 
-function navigateToPlayer() {
-    searchContainer.style.display = "none";
-    mainContainer.style.display = "none";
-    playerContainer.style.display = "flex";
-    queueContainer.style.display = "flex";
-    searchLink.classList.remove("selected");
-    listenLink.classList.add("selected");
-}
+    function navigateToPlayer() {
+        searchContainer.style.display = "none";
+        mainContainer.style.display = "none";
+        playerContainer.style.display = "flex";
+        queueContainer.style.display = "flex";
+        searchLink.classList.remove("selected");
+        listenLink.classList.add("selected");
+    }
+});
 
 /* const image = document.querySelector("img");
 const title = document.getElementById("title");
